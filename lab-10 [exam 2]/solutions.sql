c5e84fb24d0b6abe3c71fd04bcb4d72154eb3122
Kazari <61841068+jKazari@users.noreply.github.com>
2025-06-09 11:35:31 +0200

    Updated exam 2

--- lab-10 [exam 2]/solutions.sql
+++ lab-10 [exam 2]/solutions.sql
@@ -1,119 +1,131 @@
 -- Zadanie 1
-CREATE TABLE ksiazki (
-    id_ksiazki           NUMBER(3) PRIMARY KEY,
-    tytul                VARCHAR2(50),
-    autor                VARCHAR2(50),
-    liczba_egzemplarzy   NUMBER(3)
+CREATE TABLE pojazdy (
+	id_pojazdu NUMBER(3) PRIMARY KEY,
+    marka VARCHAR2(50),
+    model VARCHAR2(50),
+    typ VARCHAR2(50),
+    rok_produkcji NUMBER(4)
 );
 
-CREATE TABLE czytelnicy (
-    id_czytelnika   NUMBER(3) PRIMARY KEY,
-    imie            VARCHAR2(50),
-    nazwisko        VARCHAR2(50),
-    data_zapisu     DATE
+CREATE TABLE klienci (
+	id_klienta NUMBER(3) PRIMARY KEY,
+    imie VARCHAR2(50),
+    nazwisko VARCHAR2(50),
+    data_rejestracji DATE
 );
 
-CREATE TABLE wypozyczenia (
-    id_wypozyczenia     NUMBER(3) PRIMARY KEY,
-    id_ksiazki          NUMBER(3),
-    id_czytelnika       NUMBER(3),
-    data_wypozyczenia   DATE,
-    CONSTRAINT wypozyczenia_ksiazki_fk 
-        FOREIGN KEY (id_ksiazki) REFERENCES ksiazki (id_ksiazki) ON DELETE SET NULL,
-    CONSTRAINT wypozyczenia_czytelnicy_fk 
-        FOREIGN KEY (id_czytelnika) REFERENCES czytelnicy (id_czytelnika) ON DELETE SET NULL
+CREATE TABLE naprawy (
+	id_naprawy NUMBER(5),
+    id_pojazdu NUMBER(3),
+    id_klienta NUMBER(3),
+    koszt NUMBER(10),
+    data_naprawy DATE,
+    CONSTRAINT naprawy_pojazdy_fk 
+		FOREIGN KEY (id_pojazdu) REFERENCES pojazdy (id_pojazdu) ON DELETE SET NULL,
+    CONSTRAINT naprawy_klienci_fk 
+        FOREIGN KEY (id_klienta) REFERENCES klienci (id_klienta) ON DELETE SET NULL
 );
-
 
 -- Zadanie 2
 INSERT ALL
-    INTO ksiazki VALUES (100, 'Opowieść 1', 'XY ZZ', 10)
-    INTO ksiazki VALUES (101, 'Opowieść 2', 'YY ZY', 7)
-    INTO ksiazki VALUES (102, 'Opowieść 3', 'YZY XZY', 30)
+    INTO pojazdy VALUES (100, 'Toyota', 'Corolla', 'Kombi', 2005)
+    INTO pojazdy VALUES (101, 'Volkswagen', 'Golf', 'Hatchback', 2009)
+    INTO pojazdy VALUES (102, 'Mercedes', 'W123', 'Sedan', 1985)
 SELECT * FROM dual;
 
 INSERT ALL
-    INTO czytelnicy VALUES (200, 'Jan', 'Kowalski', TO_DATE('05-05-2024', 'DD-MM-YYYY'))
-    INTO czytelnicy VALUES (201, 'Piotr', 'Adamczyk', TO_DATE('06-05-2024', 'DD-MM-YYYY'))
-    INTO czytelnicy VALUES (202, 'Tomasz', 'Rozenek', TO_DATE('07-05-2024', 'DD-MM-YYYY'))
+    INTO klienci VALUES (100, 'Jan', 'Kowalski', TO_DATE('28-10-2021', 'DD-MM-YYYY'))
+    INTO klienci VALUES (101, 'Maria', 'Nowacka', TO_DATE('04-06-2023', 'DD-MM-YYYY'))
+    INTO klienci VALUES (102, 'Piotr', 'Wiśniecki', TO_DATE('21-05-2025', 'DD-MM-YYYY'))
 SELECT * FROM dual;
 
 INSERT ALL
-    INTO wypozyczenia VALUES (300, 100, 200, TO_DATE('15-05-2024', 'DD-MM-YYYY'))
-    INTO wypozyczenia VALUES (301, 100, 201, TO_DATE('17-05-2024', 'DD-MM-YYYY'))
-    INTO wypozyczenia VALUES (302, 101, 200, TO_DATE('23-05-2024', 'DD-MM-YYYY'))
+    INTO naprawy VALUES (10000, 100, 100, 6000, TO_DATE('05-05-2024', 'DD-MM-YYYY'))
+    INTO naprawy VALUES (10001, 101, 101, 4000, TO_DATE('17-05-2025', 'DD-MM-YYYY'))
+    INTO naprawy VALUES (10002, 102, 102, 12000, TO_DATE('24-05-2025', 'DD-MM-YYYY'))
+	INTO naprawy VALUES (10003, 102, 102, 500, TO_DATE('01-06-2025', 'DD-MM-YYYY'))
 SELECT * FROM dual;
-
 
 -- Zadanie 3
-SELECT k.tytul, COUNT(w.id_wypozyczenia) AS liczba_wypozyczen
-FROM ksiazki k
-JOIN wypozyczenia w ON k.id_ksiazki = w.id_ksiazki
-GROUP BY k.tytul
-HAVING COUNT(w.id_wypozyczenia) >= 2;
-
+SELECT
+	p.marka,
+	p.model,
+	k.imie,
+	k.nazwisko,
+	n.data_naprawy,
+	n.koszt
+FROM
+	naprawy n
+	JOIN pojazdy p ON n.id_pojazdu = p.id_pojazdu
+	JOIN klienci k ON n.id_klienta = k.id_klienta;
 
 -- Zadanie 4
-SELECT autor, COUNT(*) AS liczba_ksiazek
-FROM ksiazki
-GROUP BY autor
-ORDER BY liczba_ksiazek DESC;
+DELETE FROM klienci WHERE id_klienta = 101;
 
+SELECT * FROM naprawy;
+-- Po usunięciu klienta o id_klienta = 101, w tabeli naprawy kolumna id_klienta dla powiązanych napraw została ustawiona na NULL (ze względu na ON DELETE SET NULL w definicji klucza obcego).
 
 -- Zadanie 5
-CREATE OR REPLACE VIEW ksiazki_popularne AS
-SELECT * FROM ksiazki
-WHERE liczba_egzemplarzy > 2;
+CREATE OR REPLACE VIEW naprawy_widok AS
+SELECT * FROM naprawy
+WHERE koszt > 1000;
 
-CREATE OR REPLACE VIEW ksiazki_popularne_spr AS
-SELECT * FROM ksiazki
-WHERE liczba_egzemplarzy > 2
-WITH CHECK OPTION CONSTRAINT ksiazki_popularne_ck;
+CREATE OR REPLACE VIEW naprawy_widok_spr AS
+SELECT * FROM naprawy
+WHERE koszt > 1000
+WITH CHECK OPTION CONSTRAINT naprawy_widok_ck;
 
-INSERT INTO ksiazki_popularne VALUES (10, 'Cień Wiatru', 'Zafón', 5);
-INSERT INTO ksiazki_popularne VALUES (11, 'Bezsenność', 'King', 1);       -- za mało egzemplarzy
-INSERT INTO ksiazki_popularne VALUES (12, 'Rok 1984', 'Orwell', 4);
-INSERT INTO ksiazki_popularne VALUES (13, 'Opowieść podręcznej', 'Atwood', 1); -- za mało egzemplarzy
+INSERT INTO naprawy_widok VALUES (10004, 101, 101, 3000, TO_DATE('01-06-2025', 'DD-MM-YYYY'));
+INSERT INTO naprawy_widok VALUES (10005, 102, 102, 100, TO_DATE('03-06-2025', 'DD-MM-YYYY')); -- nie spełnia warunku
 
-INSERT INTO ksiazki_popularne_spr VALUES (14, 'YTY', 'YZYT', 6);
-INSERT INTO ksiazki_popularne_spr VALUES (15, 'XYZ', 'YTXX', 1); -- nie spełnia warunku widoku
+-- Dodanie obu recordów się udało ponieważ perspektywa `naprawy_widok` 
+-- nie zawiera opcji `with check`
 
-SELECT * FROM ksiazki;
-SELECT * FROM ksiazki_popularne;
-SELECT * FROM ksiazki_popularne_spr;
+INSERT INTO naprawy_widok_spr VALUES (10006, 100, 100, 350, TO_DATE('04-06-2025', 'DD-MM-YYYY')); -- nie spełnia warunku
+INSERT INTO naprawy_widok_spr VALUES (10007, 101, 101, 4000, TO_DATE('07-06-2025', 'DD-MM-YYYY'));
 
+-- Dodanie pierwszego recordu się nie udało ponieważ naprawa
+-- nie spełnia warunku `koszt > 1000`
+
+SELECT * FROM naprawy_widok;
+SELECT * FROM naprawy_widok_spr;
+SELECT * FROM naprawy;
 
 -- Zadanie 6
-CREATE OR REPLACE VIEW aktywni_czytelnicy AS
-SELECT 
-    r.imie, 
-    r.nazwisko, 
-    COUNT(w.id_wypozyczenia) AS liczba_wypozyczen
-FROM czytelnicy r
-JOIN wypozyczenia w ON r.id_czytelnika = w.id_czytelnika
-GROUP BY r.imie, r.nazwisko
-HAVING COUNT(w.id_wypozyczenia) > 3;
-
-SELECT * FROM aktywni_czytelnicy;
-
+SELECT
+	k.imie,
+	k.nazwisko,
+	COUNT(*) AS liczba_napraw
+FROM
+	naprawy n
+	JOIN klienci k ON n.id_klienta = k.id_klienta
+GROUP BY
+	k.imie,
+	k.nazwisko
+HAVING
+	COUNT(*) > 1;
 
 -- Zadanie 7
-CREATE OR REPLACE VIEW wypozyczenia_czytelnikow AS
-SELECT 
-    r.imie, 
-    r.nazwisko, 
-    k.tytul, 
-    w.data_wypozyczenia AS wypozyczone_ksiazki
-FROM czytelnicy r
-JOIN wypozyczenia w ON r.id_czytelnika = w.id_czytelnika
-JOIN ksiazki k ON w.id_ksiazki = k.id_ksiazki;
+SELECT
+	p.marka,
+	p.model,
+	k.imie,
+	k.nazwisko,
+	n.data_naprawy,
+	n.koszt
+FROM
+	naprawy n
+	JOIN pojazdy p ON n.id_pojazdu = p.id_pojazdu
+	JOIN klienci k ON n.id_klienta = k.id_klienta
+WHERE
+	p.marka = 'Toyota'
+	AND EXTRACT(YEAR FROM n.data_naprawy) = 2024;
 
-SELECT tytul 
-FROM wypozyczenia_czytelnikow
-WHERE imie = 'Jan' 
-  AND nazwisko = 'Kowalski' 
-  AND EXTRACT(YEAR FROM wypozyczone_ksiazki) = 2024;
-
-SELECT COUNT(*) 
-FROM wypozyczenia
-WHERE EXTRACT(YEAR FROM data_wypozyczenia) = 2025;
+SELECT
+	SUM(n.koszt) AS laczny_koszt
+FROM
+	naprawy n
+	JOIN pojazdy p ON n.id_pojazdu = p.id_pojazdu
+WHERE
+	p.marka = 'Toyota'
+	AND EXTRACT(YEAR FROM n.data_naprawy) = 2024;
